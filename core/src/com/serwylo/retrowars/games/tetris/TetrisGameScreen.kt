a7288eb720df0abb4644fe8be77f1d010b5c5858
MageDelfador <9780339+MageDelfador@users.noreply.github.com>
2026-08-08 19:42:30 +0800

    Update TetrisGameScreen.kt

--- core/src/com/serwylo/retrowars/games/tetris/TetrisGameScreen.kt
+++ core/src/com/serwylo/retrowars/games/tetris/TetrisGameScreen.kt
@@ -26,7 +26,7 @@ class TetrisGameScreen(game: RetrowarsGame) : GameScreen(game, Games.tetris, 400
         const val TAG = "TetrisGameScreen"
     }
 
-    private var state = TetrisGameState()
+    private val state = TetrisGameState(RetrowarsClient.get()?.gameSeed)
     private val sounds = TetrisSoundLibrary()
     override fun getSoundLibrary() = sounds
 
